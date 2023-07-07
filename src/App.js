1df6d54c342fcfee95b2f9ae40ff0b8d55c515d0
github sunghankwon <github akvo852@naver.com>
2023-07-07 11:49:30 +0900

    Link 태그 적용

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
 import MainpageComponent from "./main/index.js";
+import { Switch, Route } from "react-router-dom";
+import UploadPage from "./upload";
+import ProductPage from "./product";
 
 function App() {
-  return <MainpageComponent />;
+  return (
+    <div>
+      <Switch>
+        <Route exact={true} path="/">
+          <MainpageComponent />
+        </Route>
+        <Route exact={true} path="/products/:id">
+          <ProductPage />
+        </Route>
+        <Route exact={true} path="/upload">
+          <UploadPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
